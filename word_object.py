3d8f2e6078a6037517fce17f9191fce372c55ed3
Antoine Voituron <avoitu@orange.fr>
2024-01-25 10:06:17 +0100

    functionning object

--- word_object.py
+++ word_object.py
@@ -1,6 +1,5 @@
+d_vowels=["A","E","I","O","U","Y","a","e","i","o","u","y"]
 class wordObject:
-    
-    d_vowels=["A","E","I","O","U","Y"]
     
     def f_compt(v_word):
         #compt length
@@ -9,10 +8,12 @@ class wordObject:
     
     def f_vowels(v_word,d_vowels):
         #compt vowels in the word
+        v_vowels=0
         for letter in v_word:
             for vowel in d_vowels:
                 if letter==vowel:
                     v_vowels=v_vowels+1
+
 
         return v_vowels
 
@@ -20,14 +21,13 @@ class wordObject:
     def f_return_word(v_word):
         v_word=v_word[::-1]
 
-def main(d_vowels):
+def main():
     v_word=input()
-    word_obj=wordObject(v_word)
     v_word_length=wordObject.f_compt(v_word)
     v_vowels=wordObject.f_vowels(v_word, d_vowels)
 
     print (v_word_length)
     print (v_vowels)
 
-if __name__=="main":
+if __name__ == "__main__":
     main()
